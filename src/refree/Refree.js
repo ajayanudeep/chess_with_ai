8ccefc3ccc0ca16761e691c0ed35b817830366c1
urstrulymahesh44 <kmrreddy44@gmail.com>
2022-01-01 07:11:13 +0530

    Made changes to Rook, Bishop and added movents to Queen and King

--- src/refree/Refree.js
+++ src/refree/Refree.js
@@ -35,7 +35,8 @@ export default class Refree {
         return false;
     }
     isValidMove(px,py,x,y,type,team,boardState){
-        console.log(type, px, py, x, y,team);
+        console.log(type,px, py, x, y,team);
+        //Pawn
         if(type === "PAWN"){
             const specialRow = (team === "w") ? 1: 6;
             const pawnDirection = (team === "w")? 1: -1;
@@ -67,6 +68,7 @@ export default class Refree {
                 return false;
             }
         }
+        //Knight
         else if(type === "KNIGHT"){
             if((y === py+2 || y === py-2) && (x === px+1 || x === px-1)){
                 if(this.tileIsOccupied(x,y,boardState)){
@@ -87,36 +89,43 @@ export default class Refree {
                 return true;
             }
         }
+        //Bishop
         else if(type === "BISHOP"){
-            let val = true;
+            let valid = true;
             if( px === x || py === y){
-                val = false;
+                valid = false;
             }
             if(x > px){
                 if(y > py){
                     for (let i = 1; i < x-px; i++) {
                         if(this.tileIsOccupied(px+i,py+i,boardState)){
-                            val = false;
+                            valid = false;
                         }
                     }
-                    if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && val === true){
-                        val = true;
+                    if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                        valid =true;
+                    }
+                    else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                        valid = true;
                     }
                     else{
-                        val = false;
+                        valid = false;
                     }
                 }
                 else if(y < py){
                     for (let i = 1; i < x-px; i++) {
                         if(this.tileIsOccupied(px+i,py-i,boardState)){
-                            val = false;
+                            valid = false;
                         }
                     }
-                    if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && val === true){
-                        val = true;
+                    if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                        valid =true;
+                    }
+                    else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                        valid = true;
                     }
                     else{
-                        val = false;
+                        valid = false;
                     }
                 }
             }
@@ -124,63 +133,231 @@ export default class Refree {
                 if(y > py){
                     for (let i = 1; i < px -x; i++) {
                         if(this.tileIsOccupied(px-i,py+i,boardState)){
-                            val = false;      
+                            valid = false;      
                         }
                     }
-                    if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && val === true){
-                        val = true;
+                    if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                        valid =true;
+                    }
+                    else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                        valid = true;
                     }
                     else{
-                        val = false;
+                        valid = false;
                     }
                 }
                 else if(y < py){
                     for (let i = 1; i <px-x; i++) {
                         if(this.tileIsOccupied(px-i,py-i,boardState)){
-                            val = false;
+                            valid = false;
                         }
                     }
-                    if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && val === true){
-                        val = true;
+                    if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                        valid =true;
+                    }
+                    else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                        valid = true;
                     }
                     else{
-                        val = false;
+                        valid = false;
                     }
                 }
             }
-            return val;
+            return valid;
         }
+        //Rook
         else if(type === "ROOK"){
-            let val = true;
+            let valid = true;
             if ( px === x ){
                 for(let i = 1; i < Math.abs(y-py); i++){
-                    if(this.tileIsOccupied(px , (py+ (i* ((y-py)/Math.abs(y-py)))),boardState)){
-                        val = false;
+                    if(this.tileIsOccupied(px , (py+ (i*((y-py)/Math.abs(y-py)))),boardState)){
+                        
+                        valid = false;
                     }
                 }
-                if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && val === true){
-                    val = true;
+                if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                    
+                    valid =true;
+                }
+                else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                    
+                    valid = true;
                 }
                 else{
-                    val = false;                }
+                    valid = false;
+                }    
             }
             else if( py === y){
                 for(let i = 1; i < Math.abs(x-px); i++){
                     if(this.tileIsOccupied(px + (i* ((x-px)/Math.abs(x-px))) , py,boardState)){
-                        val = false;
+                        valid = false;
                     }
                 }
-                if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && val === true){
-                    val = true;
+                if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                    valid =true;
+                }
+                else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                    valid = true;
                 }
                 else{
-                    val = false;
+                    valid = false;
                 }
             }
             else{
-                val =false;
+                valid =false;
             }
-            return val;
+            return valid;
+        }
+        // Queen
+        else if(type === "QUEEN"){
+            let valid = true;
+            if ( px === x ){
+                for(let i = 1; i < Math.abs(y-py); i++){
+                    if(this.tileIsOccupied(px , (py+ (i*((y-py)/Math.abs(y-py)))),boardState)){
+                        
+                        valid = false;
+                    }
+                }
+                if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                    
+                    valid =true;
+                }
+                else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                    
+                    valid = true;
+                }
+                else{
+                    valid = false;
+                }    
+            }
+            else if( py === y){
+                for(let i = 1; i < Math.abs(x-px); i++){
+                    if(this.tileIsOccupied(px + (i* ((x-px)/Math.abs(x-px))) , py,boardState)){
+                        valid = false;
+                    }
+                }
+                if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                    valid =true;
+                }
+                else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                    valid = true;
+                }
+                else{
+                    valid = false;
+                }
+            }
+            else if(x > px){
+                if(y > py){
+                    for (let i = 1; i < x-px; i++) {
+                        if(this.tileIsOccupied(px+i,py+i,boardState)){
+                            valid = false;
+                        }
+                    }
+                    if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                        valid =true;
+                    }
+                    else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                        valid = true;
+                    }
+                    else{
+                        valid = false;
+                    }
+                }
+                else if(y < py){
+                    for (let i = 1; i < x-px; i++) {
+                        if(this.tileIsOccupied(px+i,py-i,boardState)){
+                            valid = false;
+                        }
+                    }
+                    if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                        valid =true;
+                    }
+                    else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                        valid = true;
+                    }
+                    else{
+                        valid = false;
+                    }
+                }
+            }
+            else if (x <px){
+                if(y > py){
+                    for (let i = 1; i < px -x; i++) {
+                        if(this.tileIsOccupied(px-i,py+i,boardState)){
+                            valid = false;      
+                        }
+                    }
+                    if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                        valid =true;
+                    }
+                    else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                        valid = true;
+                    }
+                    else{
+                        valid = false;
+                    }
+                }
+                else if(y < py){
+                    for (let i = 1; i <px-x; i++) {
+                        if(this.tileIsOccupied(px-i,py-i,boardState)){
+                            valid = false;
+                        }
+                    }
+                    if (!this.tileIsOccupied(x,y,boardState) && valid ){
+                        valid =true;
+                    }
+                    else if(this.tileIsOccupiedByOpponent(x,y,boardState,team) && valid === true){
+                        valid = true;
+                    }
+                    else{
+                        valid = false;
+                    }
+                }
+            }
+            else{
+                valid = false;
+            }
+            return valid;
+        }
+        //King
+        else if(type === "KING"){
+            if(px === x ){
+                if(py === y+1 || py === y-1){
+                    if(!this.tileIsOccupied(x,y,boardState)){
+                        return true;
+                    }
+                    else if(!this.tileIsOccupiedByOpponent(x,y,boardState,team)){
+                        return false;
+                    }
+                    else{
+                        return true;
+                    }
+                }
+            }
+            else if(py === y ){
+                if(px === x+1 || px === x-1){
+                    if(!this.tileIsOccupied(x,y,boardState)){
+                        return true;
+                    }
+                    else if(!this.tileIsOccupiedByOpponent(x,y,boardState,team)){
+                        return false;
+                    }
+                    else{
+                        return true;
+                    }
+                }
+            }
+            else if((py === y+1 && px === x+1)  || (py === y-1 && px === x-1) || (py === y+1 && px === x-1) || (py === y-1 && px === x+1)){
+                if(!this.tileIsOccupied(x,y,boardState)){
+                    return true;
+                }
+                else if(!this.tileIsOccupiedByOpponent(x,y,boardState,team)){
+                    return false;
+                }
+                else{
+                    return true;
+                }
+            }
         }
         return false;
     }
