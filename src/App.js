6deefd56c00b9d3ebc35433ad27ef44df8972f0b
ajayanudeep <ajaykumarvelangi1@gmail.com>
2022-01-21 22:10:43 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Chessboard from './components/Chessboard';
 import './App.css';
 import Start from './components/Start';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import React from 'react';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Route path="/"><Start /></Route> 
-        <Route path="/game"><Chessboard /></Route>
-      {/* <Start></Start>
-      <Chessboard /> */}
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Start />}></Route> 
+          <Route path="game/"element={<Chessboard />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
