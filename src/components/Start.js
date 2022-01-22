79206b907d3351f6bc5870377c660712ce1ee789
urstrulymahesh44 <kmrreddy44@gmail.com>
2022-01-22 11:07:15 +0530

    added routing

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Start() {
     console.log("moce")
     return (
-        <div className='Startup'>
-            start
+        <div className='Startup'> 
+            <h1 className='Heading'>CHESS MASTER</h1>
+            <button className='btn'>
+                <Link to="/game" className='link'>Start Game</Link></button>
         </div>
     )
 }
+
+
 
 export default Start
